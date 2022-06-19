dfb2e3f54b178a9e1a4c0e78ec6ff1f7a8243a9b
Scott Lee <scottlee5455@outlook.com>
2022-06-19 06:09:50 -0800

    tying in the state machine with the input reader

--- Treaters/Assets/Scripts/StateMachine/Player/PlayerTestState.cs
+++ Treaters/Assets/Scripts/StateMachine/Player/PlayerTestState.cs
@@ -1,30 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    private float timer = 5f;
+    private float timer;
 
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
-        Debug.Log("enter");
+        stateMachine.InputReader.MoveEvent += OnMove;
     }
 
     public override void Tick(float deltaTime)
     {
-        timer -= deltaTime;
+        timer += deltaTime;
         Debug.Log(timer);
-        if(timer <= 0f)
-        {
-            stateMachine.SwitchState(new PlayerTestState(stateMachine));
-        }
     }
 
     public override void Exit()
     {
-        Debug.Log("Exit");
+        stateMachine.InputReader.MoveEvent -= OnMove;
+    }
+
+    private void OnMove()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
 }
